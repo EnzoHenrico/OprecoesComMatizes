0a78d84f36e3465031211b4070257cab3b8bb3ea
EnzoHenrico <enzo.mascia1@gmail.com>
2024-04-25 09:38:07 -0300

    feat: Convertido o menu para funcao

--- Matizes/Program.cs
+++ Matizes/Program.cs
@@ -14,9 +14,90 @@ void solicitarTamanhoDasMatrizes()
         if (tamanhoColuna < 1 || tamanhoLinha < 1)
         {
             Console.WriteLine("Os valores devem ser maiores que 0. Tente novamente.\n");
+        } 
+        else
+        {
+            Console.Clear();
         }
 
     } while (tamanhoColuna < 1 || tamanhoLinha < 1);
+}
+
+void menu()
+{
+    // Interação inicial
+    solicitarTamanhoDasMatrizes();
+
+    float[,] matriz1 = gerarMatrizAleatoria(tamanhoLinha, tamanhoColuna);
+    float[,] matriz2 = gerarMatrizAleatoria(tamanhoLinha, tamanhoColuna);
+
+    // Matrizes iniciais
+    imprimirMatriz(matriz1, "Matriz 1:");
+    imprimirMatriz(matriz2, "Matriz 2:");
+
+    do
+    {
+        Console.WriteLine("\nSelecione a opção desejada: \n");
+        Console.WriteLine("1 - Soma");
+        Console.WriteLine("2 - Subtração");
+        Console.WriteLine("3 - Multiplicação");
+        Console.WriteLine("4 - Divisão");
+        Console.WriteLine("5 - Gerar novos valores");
+
+        opcaoDoUsuario = int.Parse(Console.ReadLine());
+        Console.Clear();
+        imprimirMatriz(matriz1, "Matriz 1:");
+        imprimirMatriz(matriz2, "Matriz 2:");
+
+        switch (opcaoDoUsuario)
+        {
+            case 1:
+                imprimirMatriz(calcularMatrizes(matriz1, matriz2, "soma"), "Resultado da soma: ");
+                break;
+            case 2:
+                imprimirMatriz(calcularMatrizes(matriz1, matriz2, "subtracao"), "Resultado da subtração: ");
+                break;
+            case 3:
+                imprimirMatriz(calcularMatrizes(matriz1, matriz2, "multiplicacao"), "Resultado da multiplicação: ");
+                break;
+            case 4:
+                imprimirMatriz(calcularMatrizes(matriz1, matriz2, "divisao"), "Resultado da divisão: ");
+                break;
+            case 5:
+                Console.Write("\nManter mesmos tamanhos? (s)Sim / (n)Não : ");
+                char input = char.Parse(Console.ReadLine());
+                if (input == 'n' || input == 'N')
+                {
+                    solicitarTamanhoDasMatrizes();
+                }
+
+                Console.Clear();
+                matriz1 = gerarMatrizAleatoria(tamanhoLinha, tamanhoColuna);
+                matriz2 = gerarMatrizAleatoria(tamanhoLinha, tamanhoColuna);
+                
+                imprimirMatriz(matriz1, "Matriz 1:");
+                imprimirMatriz(matriz2, "Matriz 2:");
+                Console.WriteLine("\nValores alterados!\n");
+                break;
+            default:
+                Console.WriteLine("Opção inválida.\n");
+                break;
+        }
+
+        if (opcaoDoUsuario != 5)
+        {
+            Console.Write("\nVoltar ao menu de opções? (s)Sim / (n)Não : ");
+            char input = char.Parse(Console.ReadLine());
+            if (input == 's' || input == 'S')
+            {
+                opcaoDoUsuario = 0;
+                Console.Clear();
+                imprimirMatriz(matriz1, "Matriz 1:");
+                imprimirMatriz(matriz2, "Matriz 2:");
+            }
+        }
+    } while (opcaoDoUsuario > 4 || opcaoDoUsuario < 1);
+
 }
 
 float[,] gerarMatrizAleatoria(int tamanhoLinha, int tamanhoColuna)
@@ -68,7 +149,7 @@ float[,] calcularMatrizes(float[,] matriz1, float[,] matriz2, string operacao)
 
 void imprimirMatriz(float[,] matriz, string nomeDaOperacao = "")
 {
-    Console.WriteLine($"{nomeDaOperacao}");
+    Console.WriteLine($"\n{nomeDaOperacao}");
     for (int linha = 0; linha < tamanhoLinha; linha++)
     {
         for (int coluna = 0; coluna < tamanhoColuna; coluna++)
@@ -79,66 +160,5 @@ void imprimirMatriz(float[,] matriz, string nomeDaOperacao = "")
     }
 }
 
-// Interação inicial
-solicitarTamanhoDasMatrizes();
-float[,] matriz1 = gerarMatrizAleatoria(tamanhoLinha, tamanhoColuna);
-float[,] matriz2 = gerarMatrizAleatoria(tamanhoLinha, tamanhoColuna);
-Console.WriteLine();
-
-// Matrizes originais
-imprimirMatriz(matriz1, "Valores originais:");
-imprimirMatriz(matriz2);
-
-do
-{
-    Console.WriteLine("\nSelecione a operação desejada: \n");
-    Console.WriteLine("1 - Soma");
-    Console.WriteLine("2 - Subtração");
-    Console.WriteLine("3 - Multiplicação");
-    Console.WriteLine("4 - Divisão");
-    Console.WriteLine("Ou:");
-    Console.WriteLine("5 - Gerar novos valores");
-    opcaoDoUsuario =  int.Parse(Console.ReadLine());
-    Console.WriteLine();
-
-    switch (opcaoDoUsuario)
-    {
-        case 1:
-            imprimirMatriz(calcularMatrizes(matriz1, matriz2, "soma"), "Soma: ");
-            break;
-        case 2:
-            imprimirMatriz(calcularMatrizes(matriz1, matriz2, "subtracao"), "Subtração: ");
-            break;
-        case 3:
-            imprimirMatriz(calcularMatrizes(matriz1, matriz2, "multiplicacao"), "Multiplicação: ");
-            break;
-        case 4:
-            imprimirMatriz(calcularMatrizes(matriz1, matriz2, "divisao"), "Divisão: ");
-            break;
-        case 5:
-            Console.Write("\nManter mesmos tamanhos? (s)Sim / (n)Não : ");
-            char input = char.Parse(Console.ReadLine());
-            if (input == 'n' || input == 'N')
-            {
-                solicitarTamanhoDasMatrizes();
-            }
-            matriz1 = gerarMatrizAleatoria(tamanhoLinha, tamanhoColuna);
-            matriz2 = gerarMatrizAleatoria(tamanhoLinha, tamanhoColuna);
-            imprimirMatriz(matriz1, "Novos valores:");
-            imprimirMatriz(matriz2);
-            break;
-        default:
-            Console.WriteLine("Opção inválida.\n");
-            break;
-    }
-    
-    if (opcaoDoUsuario != 5)
-    {
-        Console.Write("\nVoltar ao menu de opções? (s)Sim / (n)Não : ");
-        char input = char.Parse(Console.ReadLine());
-        if (input == 's' || input == 'S')
-        {
-            opcaoDoUsuario = 0;
-        }
-    }
-} while (opcaoDoUsuario > 4 || opcaoDoUsuario < 1);
+// Chamar menu para disparar funcionalidades
+menu();
